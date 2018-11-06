cb384f0d6369743696431db231f8a5e615a58cae
Roman <ronin911@gmail.com>
2018-11-06 20:27:06 +0200

    Median

---
+++ Python_3/median.py
@@ -0,0 +1,6 @@
+first_list_len = int(input('Type a lenght of the first list\n'))
+second_list_len = int(input('Type a lenght of the second list\n'))
+
+median = (first_list_len + second_list_len)/2
+
+print('A median of the two merged lists is {}'.format(median))
