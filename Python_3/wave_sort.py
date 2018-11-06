cfbfae84769cd59dc1ffae1b0cbbcc92353ade20
Roman <ronin911@gmail.com>
2018-11-06 18:18:14 +0200

    Wave sorting. Done in Algo basics 4

---
+++ Python_3/wave_sort.py
@@ -0,0 +1,45 @@
+#Script sorts a list of numbers with quick sort
+
+import random
+
+def quick_sort(list, first, last):
+    left = first
+    right = last
+    medium = list[round((left + right) / 2)]
+    while left <= right:
+        while list[left] < medium:
+            left += 1
+            pass
+        while list[right] > medium:
+            right -= 1
+            pass
+        if left <= right:
+            list[left], list[right] = list[right], list[left]
+            left += 1
+            right -= 1
+        pass
+    if first < right:
+        list = quick_sort(list, first, right)
+    if left < last:
+        list = quick_sort(list, left, last)
+    return list
+
+
+list_range = int(input('Type list range >'))
+random_range = int(input('Type random range >'))
+
+l = [random.randrange(random_range) for i in range(list_range)]
+print('\nInitial list\n',l,'\n')
+
+l = quick_sort(l, 0, len(l)-1)
+
+i = 1
+while i < len(l)-1:
+    l[i], l[i+1] = l[i+1], l[i]
+    if i == len(l) - 2:
+        i += 1
+    else:
+        i += 2
+    pass
+
+print('Sorted wave list\n', l)
